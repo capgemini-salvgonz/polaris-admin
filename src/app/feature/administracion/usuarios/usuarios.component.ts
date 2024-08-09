d631c7aa76267e67cb03c459a80053b6793600e4
chava.gnolasco@gmail.com <chava.gnolasco@gmail.com>
2024-08-09 10:41:45 -0600

    # 92cdaab9 - Agregar lista de usuarios en PADMIN para usuarios ADMIN
    - Option list to manage user list

--- src/app/feature/administracion/usuarios/usuarios.component.ts
+++ src/app/feature/administracion/usuarios/usuarios.component.ts
@@ -1,21 +1,28 @@
 import { Component } from '@angular/core';
+
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
 import { UserListComponent } from '../../../components/user-list/user-list.component';
 import { MainLayoutComponent } from '../../../components/main-layout/main-layout.component';
-
 import { User } from '../../../models/user.model';
 import { UserService } from '../../../services/users/user.service';
+
+// Dialogs
+import { UserSearchComponent } from '../../../components/user-search/user-search.component'
 
 @Component({
   selector: 'app-usuarios',
   standalone: true,
-  imports: [MainLayoutComponent, UserListComponent],
+  imports: [MainLayoutComponent, UserListComponent, MatIconModule, MatButtonModule],
   templateUrl: './usuarios.component.html',
-  styleUrl: './usuarios.component.css'
+  styleUrls: ['./usuarios.component.css']
 })
 export class UsuariosComponent {
   users: User[] = [];
 
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private dialog: MatDialog) {}
 
   ngOnInit(): void {
     this.userService.getUsers().subscribe({
@@ -26,5 +33,25 @@ export class UsuariosComponent {
         console.error(error);
       }
     });
+  }
+
+  openDialog(action: string): void {
+    console.log("Action: " + action);
+
+    let ref;
+
+    switch (action) {
+      case 'add':
+        ref = this.dialog.open(UserSearchComponent);
+        break;
+      case 'search':
+        this.dialog.open(UserSearchComponent);
+        break;
+      case 'export':
+        this.dialog.open(UserSearchComponent);
+        break;
+      default:
+        break;
+    }
   }
 }
