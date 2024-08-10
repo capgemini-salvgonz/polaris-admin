68a69734029b61ce98355342fec440931f4135c4
chava.gnolasco <chava.gnolasco@gmail.com>
2024-08-09 18:49:18 -0600

    Prod URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  padminHostName: 'https://api.production.com'
+  padminHostName: 'https://padmin-api-dpcphffcb4e3arhw.mexicocentral-01.azurewebsites.net'
 };
